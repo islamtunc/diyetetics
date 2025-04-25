a85f635dbb9bbacdeec8ccf2f2fd646f57e8e2be
cigerxwinislam <islamtunc748@gmail.com>
2025-04-25 10:01:29 +0300

    serxere nu

--- mela/src/app/malper/page.tsx
+++ mela/src/app/malper/page.tsx
@@ -14,9 +14,11 @@ export default function Page() {
 
 <br></br><br></br><br></br><br></br>
 
+
 Amade dibe...
 <br></br>
 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/rAJ4Yd_PjGY?si=fw6OIKWYFu7YF3Vr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   );
 }
