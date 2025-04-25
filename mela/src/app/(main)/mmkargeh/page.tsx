9d9d9fd65943136a55c1ebd993925ef4a7a23a8d
cigerxwinislam <islamtunc748@gmail.com>
2025-04-25 12:32:45 +0300

    serxere nu

--- mela/src/app/(main)/mmkargeh/page.tsx
+++ mela/src/app/(main)/mmkargeh/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
 
-        <h1 className="text-3xl font-semibold">İş ilanları</h1>
+        <h1 className="text-3xl font-semibold">Beslenme Programlarım</h1>
         <Tabs defaultValue="for-you">
           <TabsList>
-            <TabsTrigger value="for-you">İlanlar</TabsTrigger>
+            <TabsTrigger value="for-you">Paylaşılanlar</TabsTrigger>
             <TabsTrigger value="following">Kategoride Ara</TabsTrigger>
-            <TabsTrigger value="mm">Yeni ilan Ver</TabsTrigger>
+            <TabsTrigger value="mm">Yeni Gönderi Yayınla</TabsTrigger>
 
           </TabsList>
           <TabsContent value="for-you">
