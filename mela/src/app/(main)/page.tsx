1bf4e6d7b1a602edeefaa9dd419766c455f67ceb
cigerxwinislam <islamtunc748@gmail.com>
2025-04-28 15:13:03 +0300

    serxere nu

--- mela/src/app/(main)/page.tsx
+++ mela/src/app/(main)/page.tsx
@@ -1,55 +1,109 @@
 // Bismillahirrahmanirahim
+// Elhamdulillahirabbulalemin
+// Es-selatu vesselamu ala rasulina Muhammedin ve ala alihi ve sahbihi ecmain
+import React from "react";
+import { Alert, Container, Row, Col, Card, Button } from "react-bootstrap";
 
-
-
-import React from 'react'
-import { Alert } from 'react-bootstrap'
-
-function page() {
+function AdminPage() {
   return (
-    <div>
+    <Container fluid style={{ background: "#f8f9fa", minHeight: "100vh", padding: "20px" }}>
+      <Row>
+        <Col>
+          <Alert variant="success" style={{ textAlign: "center", fontSize: "18px" }}>
+            Selam Aleykum dear Customer or Developer, Welcome to Admin Panel
+          </Alert>
+        </Col>
+      </Row>
 
+      <Row className="mb-4">
+        <Col>
+          <Alert
+            style={{
+              background: "green",
+              color: "white",
+              textAlign: "center",
+              fontSize: "16px",
+            }}
+          >
+            Sernameye name nameye Allah <br />
+            Be namaye nameye kemi wi Wallah
+          </Alert>
+        </Col>
+      </Row>
 
+      <Row>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Anasayfa</Card.Title>
+              <Card.Text>Admin panelinin ana sayfasına erişim sağlayın.</Card.Text>
+              <Button variant="primary" href="/mmavahi">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Hakkımızda</Card.Title>
+              <Card.Text>Hakkımızda sayfasını düzenleyin veya görüntüleyin.</Card.Text>
+              <Button variant="primary" href="/mmkinc">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Beslenme Programları</Card.Title>
+              <Card.Text>Beslenme programlarını yönetin.</Card.Text>
+              <Button variant="primary" href="/mmkargeh">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
-
-<Alert> <Alert>Selam Aleykum dear Customer or Developer Welcome to Admin Panel </Alert>
-
-<br></br>
-
-
-<Alert style={{background:"green", color:"white",textAlign:"center"}}>Sernameye name nameye Allah <br></br> 
-
-Be namaye nameye kemi wi Wallah
-</Alert>
-</Alert>
-
-<Alert>
-
-<Alert>     <a href='/mmavahi'>Anasayfa</a> 
-</Alert>
-
-<Alert>     <a href='/mmkinc'>Hakkımızda</a> 
-</Alert>
-
-<Alert>     <a href='/mmkargeh'>Beslenme Programları</a> 
-</Alert>
-
-
-<Alert>     <a href='/mmwesayit'>Programlar</a> 
-</Alert>
-
-<Alert>     <a href='/mmkedkar'>Blog Yazılarım</a> 
-</Alert>
-
-
-</Alert>
-
-
-<Alert>     <a href='/malper'>Site</a> 
-</Alert>
-
-    </div>
-  )
+      <Row>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Programlar</Card.Title>
+              <Card.Text>Programlarınızı düzenleyin veya görüntüleyin.</Card.Text>
+              <Button variant="primary" href="/mmwesayit">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Blog Yazılarım</Card.Title>
+              <Card.Text>Blog yazılarınızı yönetin ve düzenleyin.</Card.Text>
+              <Button variant="primary" href="/mmkedkar">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={4} className="mb-3">
+          <Card>
+            <Card.Body>
+              <Card.Title>Site</Card.Title>
+              <Card.Text>Site ayarlarını düzenleyin veya görüntüleyin.</Card.Text>
+              <Button variant="primary" href="/malper">
+                Git
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default page
+export default AdminPage;
