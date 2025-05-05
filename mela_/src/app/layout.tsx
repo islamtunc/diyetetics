3e4f2f31ab6f00c559a49c57adbb90a47a1ed257
Yekazad Software Center <islamtunc748@gmail.com>
2025-05-05 13:58:08 +0300

    serxere nu

--- mela_/src/app/layout.tsx
+++ mela_/src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
         <ReactQueryProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
