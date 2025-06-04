73ab74698417590b2724baab2f2d217095418ca3
cigerxwinislam <islamtunc748@gmail.com>
2025-06-04 10:43:34 +0300

    serxere nu

--- mela_/src/app/(main)/MenuBar.tsx
+++ mela_/src/app/(main)/MenuBar.tsx
@@ -1,7 +1,8 @@
 // Bismillahirrahmanirrahim 
 // Elhamdulillahirabbulalemin
 // Esselatu vesselamu ala rasulina Muhammedin ve ala alihi ve sahbihi ecmain
-
+// Suphanallah, Elhamdulillah, Allahu Ekber
+// Allah U Ekber, Allah U Ekber, Allah U Ekber, La ilahe illallah
 
 import { validateRequest } from "@/auth";
 import { Button } from "@/components/ui/button";
