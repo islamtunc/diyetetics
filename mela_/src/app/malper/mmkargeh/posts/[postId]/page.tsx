2875fe75fa1400aa73e03966348b467d57f27587
Yekazad Software Center <91857222+islamtunc@users.noreply.github.com>
2025-06-11 22:20:52 +0300

    Update page.tsx

--- mela_/src/app/malper/mmkargeh/posts/[postId]/page.tsx
+++ mela_/src/app/malper/mmkargeh/posts/[postId]/page.tsx
@@ -3,7 +3,7 @@
 
 
 import { validateRequest } from "@/auth";
-import FollowButton from "@/components/FollowButton";
+
 import Linkify from "@/components/Linkify";
 import Post from "@/components/posts/Post";
 import UserAvatar from "@/components/UserAvatar";
